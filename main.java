2afa49f205fa5d00d80e5c0a49138cea692f6366
Ben <bimichel14@gmail.com>
2018-01-05 11:50:05 +0100

    initialisation des variables

--- main.java
+++ main.java
@@ -1,10 +1,17 @@
-public class HelloWorld
+public class main
 {	
 	public static void addition (long gauche, long droite)
 	{
 		gauche = gauche + droite;
 	}
    public static void main(String[] args) {
+	   long g = 2; 
+	   long d = 3;
+	   System.out.println("g = " );
+	   System.out.println (g);
+	   System.out.println("d = " );
+	   System.out.println(d);
+
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
    }
