21314cf0574a1c28c60a7850431cee708fb683c7
Ben <bimichel14@gmail.com>
2018-01-05 12:11:44 +0100

    creation de deux tableaux de long

--- main.java
+++ main.java
@@ -17,5 +17,6 @@ public class main
       System.out.println("modif varibles en argument");
       System.out.println("Hello, World");
       System.out.println("creation de methode statique qui additionne la case 0");
+      System.out.println("creation de deux tableaux de long");
    }
 }
